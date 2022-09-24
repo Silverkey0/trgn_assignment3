c11d30387e5e9be78b3dcda3403fa2ce68bcf7bf
Silverkey0 <yidian@usc.edu>
2022-09-23 22:31:21 -0700

    assignment3

--- scripts/histogram.py
+++ scripts/histogram.py
@@ -3,17 +3,40 @@ import pandas as pd
 import numpy as np   
 import matplotlib.pyplot as plt
 
-if sys.argv[:1] == '-f' :
-    n= str((column_selection[2]))
-    
-data = pd.read_tsv(sys.argv(2), sep='\t', header=0)
-data.head()
-    df = pd.Dataframe(data)
-        n = [1,2,3,4]
-        df = df[str(df.columns[4])]
-        ax = df.plot.hist()
+
+
+
+
+def main(argv):
+    argv_count=len(argv)
+    if argv_count>1:
+        content=""
+        dicts={}
+        number = ''
+        try:
+          opts, args = getopt.getopt(argv,"f:",["number="])
+        except getopt.GetoptError:
+          print('python3 ensg2hugo.py -f<number> <output>')
+          sys.exit(2)
+        for opt, arg in opts:
+          if opt in ("-f", "--ifile"):
+             number = arg
+
+    if os.path.exists(sys.argv[2]):
+        Titanic = pd.read_csv(sys.argv[2])
+
+        if int(number)==4:
+            Titanic.AveExpr.plot(kind = 'hist', bins = 20, color = 'steelblue', edgecolor = 'black', label ='AveExpr')
+        else:
+
+            Titanic.logFC.plot(kind = 'hist', bins = 20, color = 'steelblue', edgecolor = 'black', label = 'logFC')
         plt.xlabel('Number Ranges')
         plt.title('Histogram')
         plt.legend()
         plt.show()
         plt.saveas(gcf,'histogram.png')
+    else:
+        print(sys.argv[2]+" not exist")
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
